6613fc77b20705332bddd9fde8e0ba2c63195cf1
jecky <jkyhdy00711@gmail.com>
2020-07-08 17:31:46 +0700

    SignUp

--- app/src/main/java/com/example/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/splashscreen/LoginActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
     private Button buttonLogin;
+    private TextView daftar;
 
 
     @Override
@@ -18,15 +20,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         buttonLogin = findViewById(R.id.buttonLogin);
-
         buttonLogin.setOnClickListener(new View.OnClickListener() {
-        @Override
         public void onClick (View v){
             // action
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         }
     });
+        daftar = findViewById(R.id.daftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                // action
+                Intent intent = new Intent(LoginActivity.this, signup.class);
+                startActivity(intent);
+            }
+        });
 
 }
 }
